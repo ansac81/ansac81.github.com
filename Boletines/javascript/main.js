09208a2c8b90f8e91080cbd10dbb2ab56a58f5d3
ansac81 <ansac81@yahoo.com.mx>
2024-09-06 23:39:08 -0600

    Agregamos el proyecto boletines

---
+++ Boletines/javascript/main.js
@@ -0,0 +1,24 @@
+//obtener los elementos de la clase close
+let links = document.querySelectorAll(".close");
+//Recorrerlos
+links.forEach(function(link){
+    // Agregar un elemento click a cada uno de ellos
+    link.addEventListener("click", function(eve){
+        eve.preventDefault();
+        let content = document.querySelector('.content');
+
+        //quitarle las clases de animacion que ya tiene
+        content.classList.remove("animate__animated");
+        content.classList.remove("animate__fadeIn");
+
+        //agregarle clases para animar su salida
+        content.classList.add("animate__animated");
+        content.classList.add("animate__fadeOut");
+
+        setTimeout(function(){
+            location.href = "/";
+        }, 600);
+
+        return false;
+    });
+});
